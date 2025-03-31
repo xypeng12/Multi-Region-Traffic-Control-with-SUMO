0f3ad7c585f7416878ba38e394fb48245d76e7c2
xypeng12 <xypeng@ucdavis.edu>
2025-03-31 00:46:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from control import joint_control,backpressure_control,only_perimeter_control
 
-joint_controller=joint_control()
+joint_controller=joint_control(mfd_type='')
 joint_controller.run()
 joint_controller.RG()
 
 only_perimeter_controller=only_perimeter_control(_with_logit_route_choice=True)
-only_perimeter_controller.run()
-
-only_perimeter_controller=only_perimeter_control()
 only_perimeter_controller.run()
 
 backpressure_controller=backpressure_control(_with_logit_route_choice=True)
@@ -15,3 +12,6 @@ backpressure_controller.run()
 
 backpressure_controller=backpressure_control()
 backpressure_controller.run()
+
+only_perimeter_controller=only_perimeter_control()
+only_perimeter_controller.run()
